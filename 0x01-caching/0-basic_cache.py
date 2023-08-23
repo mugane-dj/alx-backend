e2499a52cc2b705fc5903d0ba8d3f601ff765ba1
mugane-dj <muganedev25@gmail.com>
2023-08-23 18:36:41 +0300

    0x0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Basic Cache System
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def init(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        The function `put` adds a key-value pair to the `cache_data`
+        dictionary.
+
+        :param key: The key parameter is used to identify the item in
+                    the cache.
+        :param item: The "item" parameter is the value that you want to
+                     store in the cache.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        The function `get` retrieves the value associated with a given
+        key from a cache.
+
+        :param key: The `key` parameter is the key used to retrieve a
+                    value from the cache
+        :return: The value associated with the given key in the cache_data
+                 dictionary is being returned.
+        """
+        if key:
+            return self.cache_data.get(key)
