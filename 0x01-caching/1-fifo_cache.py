c31845c1ef032d6205a488fd5dfc062ffc9a3bc4
mugane-dj <muganedev25@gmail.com>
2023-08-23 19:10:29 +0300

    0x1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,50 @@
+"""
+FIFO Caching System
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Inherits from BaseCaching class and overrides put and get method
+    using FIFO replacement policy
+    """
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        The function `put` adds a key-value pair to the `cache_data`
+        dictionary.
+
+        :param key: The key parameter is used to identify the item in
+                    the cache.
+        :param item: The "item" parameter is the value that you want to
+                     store in the cache.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            for key in self.cache_data.keys():
+                first_key = key
+                break
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """
+        The function `get` retrieves the value associated with a given
+        key from a cache.
+
+        :param key: The `key` parameter is the key used to retrieve a
+                    value from the cache
+        :return: The value associated with the given key in the cache_data
+                 dictionary is being returned.
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
