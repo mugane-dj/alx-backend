f5a26c3793d7c50a123d91ce8e6ee2bd9b4cd9eb
mugane-dj <muganedev25@gmail.com>
2023-08-29 16:02:23 +0300

    0x3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Flask web application
+"""
+from flask_babel import Babel, gettext
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Babel configs
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Configure supported languages
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/")
+def index():
+    """Renders index.html"""
+    home_title = gettext("home_title")
+    home_header = gettext("home_header")
+    return render_template(
+        "index.html", home_title=home_title, home_header=home_header
+    )
+
+
+if __name__ == "__main__":
+    app.run()
