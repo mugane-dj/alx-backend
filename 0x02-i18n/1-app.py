7a014dc4d25a341027ecf221302c1204a936f537
mugane-dj <muganedev25@gmail.com>
2023-08-29 14:06:10 +0300

    0x1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Instantiate Babel Extension
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """Renders index.html"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
