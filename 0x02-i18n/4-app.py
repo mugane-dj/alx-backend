2ed0decf2d410cd9959479f1a61e2f2a16c0cf0b
mugane-dj <muganedev25@gmail.com>
2023-08-30 18:27:12 +0300

    0x4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,11 +2,11 @@
 """
 Force locale with URL parameter
 """
-from flask_babel import Babel, gettext
+import flask_babel
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-babel = Babel(app)
+babel = flask_babel.Babel(app)
 
 
 class Config:
@@ -36,8 +36,8 @@ def get_locale():
 @app.route("/")
 def index():
     """Renders index.html"""
-    home_title = gettext("home_title")
-    home_header = gettext("home_header")
+    home_title = flask_babel.gettext("home_title")
+    home_header = flask_babel.gettext("home_header")
     return render_template(
         "3-index.html", home_title=home_title, home_header=home_header
     )
